a0be239a4698f930da6fac5189a76ad19996c71a
tshinson <tshinson@ncsu.edu>
2016-05-27 17:34:31 -0400

    testing a comment.

---
+++ TCdatabase/TCdatabase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCdatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // add a simple comment to push.
+        }
+    }
+}
