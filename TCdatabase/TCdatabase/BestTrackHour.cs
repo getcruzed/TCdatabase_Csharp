147b963988e2b5e684469d12f0fc4afdeb449550
tshinson <tshinson@ncsu.edu>
2016-05-27 22:05:33 -0400

    add hour.

---
+++ TCdatabase/TCdatabase/BestTrackHour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCdatabase
+{
+    class BestTrackHour
+    {
+
+        public string yearMonthDay;
+
+        public string hourInUTC;
+
+        public string recordIdentifier;
+
+        public string statusOfSystem;
+
+        public double latitude;
+
+        public double longitude;
+
+        public int maxSustainedWind;
+
+        public int minimumPressure;
+
+        public int windRadii34_NE;
+        public int windRadii34_SE;
+        public int windRadii34_SW;
+        public int windRadii34_NW;
+
+        public int windRadii50_NE;
+        public int windRadii50_SE;
+        public int windRadii50_SW;
+        public int windRadii50_NW;
+
+        public int windRadii64_NE;
+        public int windRadii64_SE;
+        public int windRadii64_SW;
+        public int windRadii64_NW;
+
+    }
+}
